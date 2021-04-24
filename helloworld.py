af289c8546747244d100bb2414d0a425a3a8ca67
nithinchandra <nithinmarch90@gmail.com>
2021-04-24 18:09:00 +0530

    ic

---
+++ helloworld.py
@@ -0,0 +1,10 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def main():
+  return 'Hello World from Python'
+
+if __name__ == '__main__':
+  print("Hello World")
+  app.run(host="0.0.0.0", port=5000, debug=True, use_reload=True)
